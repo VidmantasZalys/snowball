9966d155c8fa2eb8d3c820230786392f7603f27a
nutonas@gmail.com <nutonas@gmail.com>
2021-02-27 23:44:46 +0200

    init

--- src/components/about/Services.js
+++ src/components/about/Services.js
@@ -1,5 +1,33 @@
+import Styled from "styled-components";
+
 const Services = () => {
-	return <div></div>;
+	return (
+		<ServicesDiv>
+			<ul>
+				<li>
+					<h1>Making Snow Balls</h1>
+					<p>We are the best making snow balls fom mountains and etc.</p>
+				</li>
+				<li>
+					<h1>Making Snow Balls</h1>
+					<p>We are the best making snow balls fom mountains and etc.</p>
+				</li>
+				<li>
+					<h1>Making Snow Balls</h1>
+					<p>We are the best making snow balls fom mountains and etc.</p>
+				</li>
+			</ul>
+		</ServicesDiv>
+	);
 };
+
+const ServicesDiv = Styled.div`
+background: white;
+ul{
+	display:flex;
+	justify-content: center;
+	list-style: none;
+}
+`;
 
 export default Services;
